6508a585fecf5ad7b2b88619ae1e5e2507c48a5c
Alex Komissarov <k.a.komissar@gmail.com>
2020-03-14 17:08:36 +0500

    Little refactor

--- pi_compute/pi_mpi.f90
+++ pi_compute/pi_mpi.f90
@@ -3,11 +3,10 @@
       include "mpif.h"
 
       CHARACTER(100)sintervals
-      INTEGER(selected_int_kind(18)) :: i, istart, iend
-      INTEGER(selected_int_kind(18)) :: intervals
-      INTEGER:: ierr, nproc, myrank
-      REAL(kind(1.d0)) :: dx,sum,x, total_sum
-      REAL(kind(1.d0)) :: f, mypi, pi
+      INTEGER :: i, start, fin
+      INTEGER :: intervals
+      INTEGER :: ierr, nproc, myrank
+      REAL(kind(1.d0)) :: mypi, pi
       REAL(kind(1.d0)), parameter :: PI25DT = acos(-1.d0)
 
       CALL MPI_INIT(ierr)
@@ -23,11 +22,11 @@
          write(*,*) "Number of intervals     = ", intervals
       endif
 
-      istart = (intervals) * myrank
-      iend   = (intervals) * (myrank + 1)
-      mypi = 0.0;
-      pi = 0.0;
-      do i = istart, iend
+      start = (intervals) * myrank
+      fin   = (intervals) * (myrank + 1)
+      mypi = 0.d0;
+      pi = 0.d0;
+      do i = start, fin
           mypi = mypi + (-1.d0)**REAL(i) / (2.d0 * REAL(i) + 1.d0)
       end do
       CALL MPI_Reduce(mypi, pi ,1,MPI_DOUBLE,MPI_SUM,0,MPI_COMM_WORLD,ierr)
